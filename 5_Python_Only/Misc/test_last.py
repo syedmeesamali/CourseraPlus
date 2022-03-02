90c094f41f729cd92390fddb9b7e0d303bdb0ad4
syedmeesamali <syedmeesamali@yahoo.com>
2022-03-02 23:41:44 +0400

    Final assignment done of PYTHON COURSE - most probably FIRST COURSERA certificate for me

--- 5_Python_Only/Misc/test_last.py
+++ 5_Python_Only/Misc/test_last.py
@@ -1,9 +1,22 @@
 largest = None
 smallest = None
 while True:
-    num = input("Enter a number: ")
-    if num == "done":
-        break
-    print(num)
-
-print("Maximum", largest)
+    try:
+        num = input("Enter a number: ")
+        if num == "done":            
+            break
+        else:
+            num = int(num)
+            if largest is None:
+                largest = num
+                smallest = num
+            else:
+                if num > largest:
+                    largest = num
+                if num < smallest:
+                    smallest = num
+    except:
+        print("Invalid input")
+        
+print("Maximum is ", largest)
+print("Minimum is ", smallest)
