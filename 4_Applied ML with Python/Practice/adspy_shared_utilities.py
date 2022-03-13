cf551a5294fa1def312ddfb1a5eab95d865195fe
syedmeesamali <syedmeesamali@yahoo.com>
2022-03-13 11:31:17 +0400

    Study and cleaning of crime data set - some further work for SVC and study of same via provided module-2 book

--- 4_Applied ML with Python/Practice/adspy_shared_utilities.py
+++ 4_Applied ML with Python/Practice/adspy_shared_utilities.py
@@ -19,9 +19,9 @@ def load_crime_dataset():
     crime = pd.read_table('CommViolPredUnnormalizedData.txt', sep=',', na_values='?')
     # remove features with poor coverage or lower relevance, and keep ViolentCrimesPerPop target column
     columns_to_keep = [5, 6] + list(range(11,26)) + list(range(32, 103)) + [145]  
-    crime = crime.ix[:,columns_to_keep].dropna()
+    crime = crime.iloc[:,columns_to_keep].dropna()
 
-    X_crime = crime.ix[:,range(0,88)]
+    X_crime = crime.iloc[:,range(0,88)]
     y_crime = crime['ViolentCrimesPerPop']
 
     return (X_crime, y_crime)
